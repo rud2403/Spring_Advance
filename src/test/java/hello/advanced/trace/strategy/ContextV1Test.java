b446cfc8fc8da301393c0aeda23109d6298cde35
bgm <qkrrudals2403@naver.com>
2023-02-01 09:32:11 +0900

    전략 패턴 - 예제1

--- src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
+++ src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
@@ -1,5 +1,8 @@
 package hello.advanced.trace.strategy;
 
+import hello.advanced.trace.strategy.code.strategy.ContextV1;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic1;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic2;
 import hello.advanced.trace.template.code.AbstractTemplate;
 import hello.advanced.trace.template.code.SubClassLogic1;
 import hello.advanced.trace.template.code.SubClassLogic2;
@@ -36,36 +39,18 @@ public class ContextV1Test {
     }
 
     /**
-     * 템플릿 메소드 패턴 적용
+     * 전략 패턴 사용
      */
     @Test
-    void templateMethodV1() {
-        AbstractTemplate template1 = new SubClassLogic1();
-        template1.excute();
+    void strategyV1() {
+        StrategyLogic1 strategyLogic1 = new StrategyLogic1();
+        ContextV1 contextV1 = new ContextV1(strategyLogic1);
+        contextV1.execute();
 
-        AbstractTemplate template2 = new SubClassLogic2();
-        template2.excute();
+        StrategyLogic2 strategyLogic2 = new StrategyLogic2();
+        ContextV1 contextV2 = new ContextV1(strategyLogic2);
+        contextV2.execute();
     }
 
-    @Test
-    void templateMethodV2() {
-        AbstractTemplate template1 = new AbstractTemplate() {
-            @Override
-            protected void call() {
-                log.info("비즈니스 로직1 실행");
-            }
-        };
-        log.info("클래스 이름1 = {}", template1.getClass());
-        template1.excute();
-
-        AbstractTemplate template2 = new AbstractTemplate() {
-            @Override
-            protected void call() {
-                log.info("비즈니스 로직2 실행");
-            }
-        };
-        log.info("클래스 이름1 = {}", template1.getClass());
-        template2.excute();
-    }
 
 }
